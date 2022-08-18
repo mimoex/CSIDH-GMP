4efe359283d8320d54d8eeb437b4a73a5ca9bbb7
mimoex <nijidream@protonmail.com>
2022-08-18 15:12:27 +0900

    Time measurement is the same as in Reference.

--- main.cpp
+++ main.cpp
@@ -1,91 +1,89 @@
+#include <time.h>
 #include "fp.h"
 
 bool csidh()
 {
-	mpz_class mod, A;
+	mpz_class mod, A;	//A=0 ( y^2 =x^3 +0*x^2 +x )
+	clock_t t0, t1;
 
 	mod = "5326738796327623094747867617954605554069371494832722337612446642054009560026576537626892113026381253624626941643949444792662881241621373288942880288065659";
 
 	//Aさんのstep1
-	chrono::system_clock::time_point start, end;
-	start = std::chrono::system_clock::now();
-
 	seckey secA;
+
+	t0 = clock();
 	genCSIDHkey(&secA);
+	t1 = clock();
 	cout << "Aさんの秘密鍵:" << endl;
 	for(int i=0;i<N;i++) cout << secA.e[i] << ", ";
 	cout << endl; 
 
-	end = std::chrono::system_clock::now();
-	double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-	cout << "Aさんの秘密鍵生成時間: " << elapsed << "\n" << endl;
+	cout << "Aさんの秘密鍵生成時間: " << 1000.000 * (t1 - t0) / CLOCKS_PER_SEC << "ms\n" << endl;
 
 	//---
-	
-	start = std::chrono::system_clock::now();
 
 	mpz_class A_parm;
 
+	t0 = clock();
 	A_parm = action(A, secA, mod);
+	t1 = clock();
+
 	cout << "Aさんの公開情報:" << A_parm << endl;
 	cout << "supersingular?: " << validate(A_parm, mod) << endl;
 
-	end = std::chrono::system_clock::now();
-	elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-	cout << "Aさんの公開鍵生成時間: " << elapsed << "\n" << endl;
+	cout << "Aさんの公開鍵生成時間: " << 1000.000 * (t1 - t0) / CLOCKS_PER_SEC << "ms\n"<< endl;
 
 
 	//Bさんのstep1
-	start = std::chrono::system_clock::now();
-
 	seckey secB;
+
+	t0 = clock();
 	genCSIDHkey(&secB);
+	t1 = clock();
+
 	cout << "Bさんの秘密鍵:" << endl;
 	for (int i = 0; i < N; i++) cout << secB.e[i] << ", ";
 	cout << endl;
 
-	end = std::chrono::system_clock::now();
-	elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-	cout << "Bさんの秘密鍵生成時間: " << elapsed << "\n" << endl;
+	cout << "Bさんの秘密鍵生成時間: " << 1000.000 * (t1 - t0) / CLOCKS_PER_SEC << "ms\n" << endl;
 
-
-
-	start = std::chrono::system_clock::now();
 
 	mpz_class B_parm;
 
+	t0 = clock();
 	B_parm = action(A, secB, mod);
+	t1 = clock();
+
 	cout << "Bさんの公開情報:" << B_parm << endl;
 	cout << "supersingular?: " << validate(B_parm, mod) << endl;
 	cout << endl;
 
-	end = std::chrono::system_clock::now();
-	elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-	cout << "Bさんの公開鍵生成時間: " << elapsed << "\n" << endl;
+	cout << "Bさんの公開鍵生成時間: " << 1000.000 * (t1 - t0) / CLOCKS_PER_SEC << "ms\n" << endl;
 
 
 
 	//Aさんのstep2
-	start = std::chrono::system_clock::now();
 	mpz_class A_sec;
+
+	t0 = clock();
 	A_sec = action(B_parm, secA, mod);
+	t1 = clock();
+
 	cout << "共有値(Aさん):" << A_sec << endl;
 	cout << endl;
 
-	end = std::chrono::system_clock::now();
-	elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-	cout << "Aさんの共有値を求める時間: " << elapsed << "\n" << endl;
+	cout << "Aさんの共有値を求める時間: " << 1000.000 * (t1 - t0) / CLOCKS_PER_SEC << "ms\n" << endl;
 
 	//Bさんのstep2
-	start = std::chrono::system_clock::now();
 	mpz_class B_sec;
+
+	t0 = clock();
 	B_sec = action(A_parm, secB, mod);
+	t1 = clock();
 	cout << "共有値(Bさん):" << B_sec << endl;
 	cout << endl;
 
-	end = std::chrono::system_clock::now();
-	elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-	cout << "Bさんの共有値を求める時間: " << elapsed << "\n" << endl;
+	cout << "Bさんの共有値を求める時間: " << 1000.000 * (t1 - t0) / CLOCKS_PER_SEC << "ms\n" << endl;
 
 
 	//check
