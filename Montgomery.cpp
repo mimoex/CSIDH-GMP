b0f6bfe76029942e5687691b2d20b6b08b6d6598
mimoex <nijidream@protonmail.com>
2022-09-09 16:17:17 +0900

    点のスカラー倍算をMontgomeryの世界で計算

--- Montgomery.cpp
+++ Montgomery.cpp
@@ -45,56 +45,58 @@ Point xDBLmon(const Point& Pm, const Point& Ap24m)
 
 
 //動かない…
-void xDBLADDmon(const Point& P, const Point& Q, const Point& R, const Point& Ap24_1,
+void xDBLADDmon(const Point& Pm, const Point& Qm, const Point& Rm, const Point& Ap24m,
 	Point& DBLout, Point& ADDout) {
 	mpz_class t0, t1, t2;
-	Point Pm, Qm, Rm, Ap24_1m, DBLoutm, ADDoutm;
-	
-	Pm.X = MR(P.X);
-	Pm.Z = MR(P.Z);
-	Qm.X = MR(Q.X);
-	Qm.Z = MR(Q.Z);
-	Rm.X = MR(R.X);
-	Rm.Z = MR(R.Z);
-	Ap24_1m.X = MR(Ap24_1.X);
+	//Point Pm, Qm, Rm, Ap24m;
+	//
+	//Pm.X = MR(P.X *para.R2);
+	//Pm.Z = MR(P.Z * para.R2);
+	//Qm.X = MR(Q.X * para.R2);
+	//Qm.Z = MR(Q.Z * para.R2);
+	//Rm.X = MR(R.X * para.R2);
+	//Rm.Z = MR(R.Z * para.R2);
+	//Ap24m.X = MR(Ap24.X * para.R2);
 	
 	add_fp(Pm.X, Pm.Z, &t0);
 	sub_fp(Pm.X, Pm.Z, &t1);
 	//sqr_fp(t0, &DBLout.X);
-	mul_mon(t0, t0, &DBLoutm.X);
+	mul_mon(t0, t0, &DBLout.X);
 	sub_fp(Qm.X, Qm.Z, &t2);
-	add_fp(Qm.X, Qm.Z, &ADDoutm.X);
+	add_fp(Qm.X, Qm.Z, &ADDout.X);
 	//mul_fp(t0, t2, &t0);
 	mul_mon(t0, t2, &t0);
 	//sqr_fp(t1, &DBLout.Z);
-	mul_mon(t1, t1, &DBLoutm.Z);
+	mul_mon(t1, t1, &DBLout.Z);
+
+	//OK
 
 	//mul_fp(t1, ADDout.X, &t1);
-	mul_mon(t1, ADDoutm.X, &t1);
-	sub_fp(DBLoutm.X, DBLoutm.Z, &t2);
+	mul_mon(t1, ADDout.X, &t1);
+	sub_fp(DBLout.X, DBLout.Z, &t2);
 	//mul_fp(DBLout.X, DBLout.Z, &DBLout.X);
-	mul_mon(DBLoutm.X, DBLoutm.Z, &DBLoutm.X);
-	//mul_fp(Ap24_1.X, t2, &ADDout.X);
-	mul_mon(Ap24_1m.X, t2, &ADDoutm.X);
-	sub_fp(t0, t1, &ADDoutm.Z);
-	add_fp(DBLoutm.Z, ADDoutm.X, &DBLoutm.Z);
-	add_fp(t0, t1, &ADDoutm.X);
+	mul_mon(DBLout.X, DBLout.Z, &DBLout.X);
+	//mul_fp(Ap24.X, t2, &ADDout.X);
+	mul_mon(Ap24m.X, t2, &ADDout.X);
+	sub_fp(t0, t1, &ADDout.Z);
+	add_fp(DBLout.Z, ADDout.X, &DBLout.Z);
+	add_fp(t0, t1, &ADDout.X);
 
 	//mul_fp(DBLout.Z, t2, &DBLout.Z);
-	mul_mon(DBLoutm.Z, t2, &DBLoutm.Z);
+	mul_mon(DBLout.Z, t2, &DBLout.Z);
 	//sqr_fp(ADDout.Z, &ADDout.Z);
-	mul_mon(ADDoutm.Z, ADDoutm.Z, &ADDoutm.Z);
+	mul_mon(ADDout.Z, ADDout.Z, &ADDout.Z);
 	//sqr_fp(ADDout.X, &ADDout.X);
-	mul_mon(ADDoutm.X, ADDoutm.X, &ADDoutm.X);
+	mul_mon(ADDout.X, ADDout.X, &ADDout.X);
 	//mul_fp(ADDout.Z, R.X, &ADDout.Z);
-	mul_mon(ADDoutm.Z, Rm.X, &ADDoutm.Z);
+	mul_mon(ADDout.Z, Rm.X, &ADDout.Z);
 	//mul_fp(ADDout.X, R.Z, &ADDout.X);
-	mul_mon(ADDoutm.X, Rm.Z, &ADDoutm.X);
-
-	ADDout.X = MR(ADDoutm.X);
-	ADDout.Z = MR(ADDoutm.Z);
-	DBLout.X = MR(DBLoutm.X);
-	DBLout.Z = MR(DBLoutm.Z);
+	mul_mon(ADDout.X, Rm.Z, &ADDout.X);
+	
+	//ADDout.X = MR(ADDout.X);
+	//ADDout.Z = MR(ADDout.Z);
+	//DBLout.X = MR(DBLout.X);
+	//DBLout.Z = MR(DBLout.Z);
 }
 
 void xDBLADD(const Point& P, const Point& Q, const Point& R, const Point& Ap24_1,
@@ -125,39 +127,38 @@ void xDBLADD(const Point& P, const Point& Q, const Point& R, const Point& Ap24_1
 
 //モンゴメリ曲線のスカラー倍
 Point xMUL(const Point& P, const Point& A_1, const mpz_class& n) {
-	Point x0, x1, Ap24_1, Point_temp1, Point_temp2;
+	Point x0, Ap24_1, Point_temp1, Point_temp2;
 	mpz_class div_ans;
-	Point Pm, Ap24_1m, x0m;
+	Point x0m, x1m, Pm, Ap24_1m;
 
-	x0 = P;
-	x0m.X = MR(x0.X);
-	x0m.Z = MR(x0.Z);
+	Pm.X = MR(P.X * para.R2);
+	Pm.Z = MR(P.Z * para.R2);
+
+	//x0 = P;
+	x0m.X = MR(P.X * para.R2);
+	x0m.Z = MR(P.Z * para.R2);
 	
 	//a24の計算
 	Ap24_1.Z = 1;
 	add_fp(A_1.X, 2, &Ap24_1.X);
-
+	
 	mul_fp(Ap24_1.X, para.inv4, &div_ans);
 	Ap24_1.X = div_ans;
+	
+	Ap24_1m.X = MR(Ap24_1.X * para.R2);
+	Ap24_1m.Z = MR(Ap24_1.Z * para.R2);
 
-	Pm.X = MR(P.X);
-	Pm.Z = MR(P.Z);
-	Ap24_1m.X = MR(Ap24_1.X);
-	Ap24_1m.Z = MR(Ap24_1.Z);
-
-	//x1 = xDBL(P, Ap24_1);
-	x1 = xDBLmon(Pm, Ap24_1m);
-
-	x1.X = MR(x1.X);
-	x1.Z = MR(x1.Z);
+	x1m = xDBLmon(Pm, Ap24_1m);
 
 	size_t bit_size;
 	bit_size = mpz_sizeinbase(n.get_mpz_t(), 2);
 
 	for (int i = bit_size - 2; i >= 0; i--) {
-		if (mpz_tstbit(n.get_mpz_t(), i) == 0) xDBLADD(x0, x1, P, Ap24_1, x0, x1);
-		else xDBLADD(x1, x0, P, Ap24_1, x1, x0);
+		if (mpz_tstbit(n.get_mpz_t(), i) == 0) xDBLADDmon(x0m, x1m, Pm, Ap24_1m, x0m, x1m);
+		else xDBLADDmon(x1m, x0m, Pm, Ap24_1m, x1m, x0m);
 	}
+	x0.X = MR(x0m.X);
+	x0.Z = MR(x0m.Z);
 	return x0;
 }
 
@@ -197,8 +198,10 @@ Point mont2ed(const Point& P)
 	return ed;
 }
 
-const mpz_class MR2 = mpz_class("2061108375497486669972312027415001064490813581256788537607556675121740674107862070712406989315913446194259636464745447945409324122528632250952933995334779");
-const mpz_class MR4 = mpz_class("4122216750994973339944624054830002128981627162513577075215113350243481348215724141424813978631826892388519272929490895890818648245057264501905867990669558");
+//2* para.R2
+const mpz_class MR2 = mpz_class("2754330337287350910078289762296635019340622830926948702498668159613744910020393901548090072114140920440777974898587161268428120328703823368547888429952778");
+//4* para.R2
+const mpz_class MR4 = mpz_class("181921878247078725408711906638664484611874167021175067384889677173480260014211265469288031201900587256929008153224877744193359415786273448152896571839897");
 
 //Isogeny
 void IsogenyCalc(const Point& A, const Point& P, const Point& K, const size_t& k,
@@ -209,12 +212,12 @@ void IsogenyCalc(const Point& A, const Point& P, const Point& K, const size_t& k
 	Point Am, Pm, Qm, Km, Ap24_C24m;
 	Point Aoutm, Poutm;
 
-	Am.X = MR(A.X);
-	Am.Z = MR(A.Z);
-	Pm.X = MR(P.X);
-	Pm.Z = MR(P.Z);
-	Km.X = MR(K.X);
-	Km.Z = MR(K.Z);
+	Am.X = MR(A.X * para.R2);
+	Am.Z = MR(A.Z * para.R2);
+	Pm.X = MR(P.X * para.R2);
+	Pm.Z = MR(P.Z * para.R2);
+	Km.X = MR(K.X * para.R2);
+	Km.Z = MR(K.Z * para.R2);
 
 	add_fp(Am.X, MR2, &Ap24_C24m.X);
 	Ap24_C24m.Z = MR4;
