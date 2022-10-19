bfada72328614b8b0a56bfadcebc10cc0f3af943
mimoex <nijidream@protonmail.com>
2022-10-19 10:40:26 +0900

    mpn_rshiftまで動作確認

--- fp.hpp
+++ fp.hpp
@@ -61,23 +61,33 @@ struct Fp {
     //MR    In:1024bit, Out:512bit
     static void MR(Fp& z, const FpDbl& x)
     {
+        mpz_class test;
         uint64_t temp[N * 2];
-        mpn_and_n(temp, x.buf, p.R.buf, N);
-        mpn_mul_n(temp, temp, p.nr.buf, N);
-        mpn_and_n(temp, temp, p.R.buf, N);
-        mpn_mul_n(temp, temp, p.p.buf, N);
-        mpn_add_n(temp, temp, x.buf, N);
-        mpn_rshift(temp, temp, p.nbit, N);
-        if (mpn_cmp(temp, p.p.buf, N) >= 0)
-            mpn_sub_n(temp, temp, p.p.buf, N);
-        *z.buf = *temp;
+        uint64_t temp512[N]{};
+        mpn_and_n(temp512, x.buf, p.R.buf, N * 2);
+        mpn_mul_n(temp, temp512, p.nr.buf, N);
+        mpn_and_n(temp512, temp, p.R.buf, N * 2);
+        mpn_mul_n(temp, temp512, p.p.buf, N);
+        mpn_add_n(temp, temp, x.buf, N * 2);
+
+        mpn_rshift(temp, temp, N * 2, p.nbit);  //計算が合わない…
+
+        *temp512 = *temp;
+        mpz_import(test.get_mpz_t(), N, -1, 8, 0, 0, temp512);
+        cout << test << endl;
+        cout << endl;
+
+        if (mpn_cmp(temp512, p.p.buf, N) >= 0)
+            mpn_sub_n(temp512, temp512, p.p.buf, N);
+
+        *z.buf = *temp512;
     }
 
     //掛け算 Mod p
     static void mul(Fp& z, const Fp& x, const Fp& y)
     {
         FpDbl temp;
-        mpn_mul_n(temp.buf, x.buf, y.buf, N*2);
+        mpn_mul_n(temp.buf, x.buf, y.buf, N);
         MR(z, temp);
     }
 
