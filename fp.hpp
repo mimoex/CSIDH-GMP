756d621d12e6635730aa6fe152cb68b3a1545eaf
mimoex <nijidream@protonmail.com>
2022-10-21 15:14:10 +0900

    low-level関数でのpow,inv,divの実装

--- fp.hpp
+++ fp.hpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <gmp.h>
 #include <gmpxx.h>
+#include <chrono>
 
 using namespace std;
 
 struct Fp {
     int v;
     Fp(int _v=0) : v(_v) {}
-    static const int N = 8;
+    static const int N = 8; //64bit * 8 =512bit
     uint64_t buf[N]={};
 
     struct FpDbl {
@@ -30,12 +31,9 @@ struct Fp {
     //足し算 Mod p OK
     static void add(Fp& z, const Fp& x, const Fp& y)
     {
-        mpz_class result;
         mpn_add_n(z.buf, x.buf, y.buf, N);
-
-        if (mpn_cmp(z.buf, p.buf, N) >= 0) {
+        if (mpn_cmp(z.buf, p.buf, N) >= 0)
             mpn_sub_n(z.buf, z.buf, p.buf, N);
-        }
     }
 
     //引き算 Mod p OK
@@ -44,8 +42,7 @@ struct Fp {
 
         if (mpn_cmp(x.buf, y.buf, N) >= 0) {
             mpn_sub_n(z.buf, x.buf, y.buf, N);
-        }
-        else {
+        } else {
             mpn_add_n(z.buf, x.buf, p.buf, N);
             mpn_sub_n(z.buf, z.buf, y.buf, N);
         }
@@ -55,8 +52,7 @@ struct Fp {
     //MR    In:1024bit, Out:512bit
     static void MR(Fp& z, const FpDbl& x)
     {
-        mpz_class test;
-        uint64_t temp[N * 2]{};
+        uint64_t temp[N * 2]{}; //初期化しろ！
         uint64_t temp512[N]{};
 
         mpn_and_n(temp512, x.buf, p.R.buf, N * 2);
@@ -70,26 +66,89 @@ struct Fp {
             temp512[i] = temp[i+7];
         }
 
-        if (mpn_cmp(temp512, p.p.buf, N) >= 0){
+        if (mpn_cmp(temp512, p.p.buf, N) >= 0)
             mpn_sub_n(z.buf, temp512, p.p.buf, N);
-        } else {
+        else {
             for (int i = 0; i < N; i++) {
                 z.buf[i] = temp512[i];
             }
         }
     }
 
-    //掛け算 Mod p
+    //掛け算 Mod p(1回で完結)
     static void mul(Fp& z, const Fp& x, const Fp& y)
     {
         mpz_class test;
         Fp temp512{};
         FpDbl temp{};
-        mpn_mul_n(temp.buf, x.buf, y.buf, N);
+        mpn_mul_n(temp.buf, x.buf, y.buf,N);
         MR(temp512,temp);
         mpn_mul_n(temp.buf, temp512.buf, p.R2.buf, N);
         MR(z, temp);
 
+    }
+
+    //掛け算 Mod p(Montgomeryで返す)
+    static void mul_mon(Fp& z, const Fp& x, const Fp& y)
+    {
+        Fp temp512{};
+        FpDbl temp{};
+        mpn_mul_n(temp.buf, x.buf, y.buf, N);
+        MR(z, temp);
+    }
+
+    static void pow(Fp& z, const Fp& x, const Fp& y)
+    {
+        Fp result;
+        FpDbl R2_temp, result_temp;
+        for (int i = 0; i < N; i++) {
+            R2_temp.buf[i] = p.R2.buf[i];
+        }
+        MR(result, R2_temp);
+
+        mpz_class y_mpz;
+        mpz_import(y_mpz.get_mpz_t(), Fp::N, -1, 8, 0, 0, y.buf);
+        for (int i = nbit - 1; i >= 0; i--) {
+            mpn_sqr(result_temp.buf, result.buf, N);
+            MR(result, result_temp);
+            if (mpz_tstbit(y_mpz.get_mpz_t(), i) == 1) {
+                mpn_mul_n(result_temp.buf, result.buf, x.buf, N);
+                MR(result, result_temp);
+            }
+        }
+        z = result;
+    }
+
+    static void inv(Fp& z, const Fp& x)
+    {
+        if (mpn_zero_p(x.buf, N))
+            throw std::range_error("Divided by zero.");
+        else {
+            Fp modhiku2;
+            uint64_t ni[N]{};
+            ni[0] = 2;
+            mpn_sub_n(modhiku2.buf, p.p.buf, ni,N);
+            pow(z, x, modhiku2);
+        }
+
+    }
+
+    static void div(Fp& z, const Fp& x, const Fp& y)
+    {
+        FpDbl x_temp{}, y_temp{},temp1024;
+        Fp temp,xmon,ymon;
+
+        mpn_mul_n(x_temp.buf, x.buf, p.R2.buf, N);
+        mpn_mul_n(y_temp.buf, y.buf, p.R2.buf, N);
+
+        MR(xmon, x_temp);
+        MR(ymon, y_temp);
+        inv(temp, ymon);
+        mul_mon(xmon, xmon, temp);
+        for (int i = 0; i < N; i++) {
+            temp1024.buf[i] = xmon.buf[i];
+        }
+        MR(z, temp1024);
     }
 
     static Fp p;
